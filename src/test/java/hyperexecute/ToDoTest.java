ea5e6e466878f3ef902175de980947b4aba8c5d0
Himanshu Jagdish Sheth <himanshuj@lambdatest.com>
2022-03-21 15:28:06 +0530

    [HyperExecute] Addition of JUnit test code

---
+++ src/test/java/hyperexecute/ToDoTest.java
@@ -0,0 +1,145 @@
+package hyperexecute;
+
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.junit.jupiter.api.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@Execution(ExecutionMode.CONCURRENT)
+public class ToDoTest
+{
+    /*  protected static ChromeDriver driver; */
+    protected WebDriver driver = null;
+    static String URL = "https://lambdatest.github.io/sample-todo-app/";
+    public static String status = "passed";
+    String gridURL = "@hub.lambdatest.com/wd/hub";
+    String user_name = System.getenv("LT_USERNAME") == null ? "LT_USERNAME" :
+            System.getenv("LT_USERNAME");
+    String access_key = System.getenv("LT_ACCESS_KEY") == null ? "LT_ACCESS_KEY" :
+            System.getenv("LT_ACCESS_KEY");
+
+    @BeforeAll
+    public static void start()
+    {
+        System.out.println("Running JUnit test on HyperExecute Grid");
+    }
+
+    @BeforeEach
+    public void setup()
+    {
+        System.out.println("Setting up resources to run tests on HyperExecute Grid");
+    }
+
+    public void SetUpBrowser(String browser)
+    {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        if (browser.equalsIgnoreCase("Chrome"))
+        {
+            capabilities.setCapability("browserName", "chrome");
+            capabilities.setCapability("version", "latest");
+            capabilities.setCapability("platform", "Windows 10");
+            capabilities.setCapability("build", "[Test 1] Running_Junit5Tests_In_Grid_Chrome");
+            capabilities.setCapability("name", "JUnit5Tests_Chrome");
+            capabilities.setCapability("network", true);
+            capabilities.setCapability("visual", true);
+            capabilities.setCapability("video", true);
+            capabilities.setCapability("console", true);
+        }
+        if (browser.equalsIgnoreCase("Firefox"))
+        {
+            capabilities.setCapability("browserName", "Firefox");
+            capabilities.setCapability("version", "latest");
+            capabilities.setCapability("platform", "Windows 10");
+            capabilities.setCapability("build", "[Test 2] Running_Junit5Tests_In_Grid_Firefox");
+            capabilities.setCapability("name", "JUnit5Tests_Firefox");
+            capabilities.setCapability("network", true);
+            capabilities.setCapability("visual", true);
+            capabilities.setCapability("video", true);
+            capabilities.setCapability("console", true);
+        }
+        try
+        {
+            driver = new RemoteWebDriver(new URL("https://" + user_name + ":" + access_key + gridURL), capabilities);
+        }
+        catch (MalformedURLException e)
+        {
+            System.out.println("Invalid grid URL");
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("browser")
+    public void test_ToDoApp(String browser)
+    {
+        SetUpBrowser(browser);
+
+        String methodName = Thread.currentThread()
+                .getStackTrace()[1]
+                .getMethodName();
+
+        driver.navigate().to(URL);
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+
+        try
+        {
+            /* Let's mark done first two items in the list. */
+            driver.findElement(By.name("li1")).click();
+            driver.findElement(By.name("li2")).click();
+
+            /* Let's add an item in the list. */
+            driver.findElement(By.id("sampletodotext")).sendKeys("Happy Testing at LambdaTest");
+            driver.findElement(By.id("addbutton")).click();
+
+            /* Let's check that the item we added is added in the list. */
+            String enteredText = driver.findElement(By.xpath("//span[.='Happy Testing at LambdaTest']")).getText();
+            if (enteredText.equals("Happy Testing at LambdaTest"))
+            {
+                System.out.println("JUnit demo on HyperExecute Grid is successful");
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @AfterEach
+    public void TearDownClass()
+    {
+        driver.quit();
+        System.out.println("Browser resources released");
+    }
+
+    @AfterAll
+    public static void endTest()
+    {
+        System.out.println("JUnit execution on HyperExecute Grid complete");
+    }
+
+    static Stream<Arguments> browser()
+    {
+        return Stream.of(
+                arguments("Chrome"),
+                arguments("Firefox")
+        );
+    }
+}
